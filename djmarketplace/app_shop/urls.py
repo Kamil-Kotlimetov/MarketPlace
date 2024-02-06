684a5ed66cc5ec09b70bd3faee39691f5da66284
user <user@user.ru>
2024-02-06 21:24:05 +0500

    lessons_4

--- djmarketplace/app_shop/urls.py
+++ djmarketplace/app_shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import register_view, CustomLoginView, CustomLogoutView, MainView, UserUpdateView
+from .views import register_view, CustomLoginView, CustomLogoutView, MainView, UserUpdateView, CartView, add_good_to_cart
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('reqister/', register_view, name='register'),
     path('', MainView.as_view(), name='main'),
     path('profile/<int:pk>', UserUpdateView.as_view(), name='profile'),
+    path('cart/', CartView.as_view(), name='cart'), #6 (06.02.)
+    path('add_good.<int:pk>/', add_good_to_cart, name='add_good'), #6 (06.02.)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djmarketplace/app_shop/views.py
+++ djmarketplace/app_shop/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import ListView, UpdateView
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, UpdateView, View
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from .forms import UserForm, CartAddForm
 from django.db.models import Avg
+from django.db import transaction
 from django.urls import reverse
+from django.contrib import messages
 from .models import *
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView
@@ -28,6 +33,33 @@ class MainView(ListView): #4
         context['avg_price'] = Good.objects.only('price').aggregate(avg_price=Avg('price')).get('avg_price')
         context['add_form'] = CartAddForm()
         return context
+
+class CartView(LoginRequiredMixin, View): #6 (06.02.)
+
+    login_url = 'login'
+    def get(self, *args, **kwargs):
+        try:
+            cart = GoodCart.objects.select_related('user', 'good').defer('payment_flag', 'good__activity_flag', 'good__amount').filter(user=self.request.user, payment_flag='n')
+            total_price = sum([i_item.good.price * i_item.good_num for i_item in cart])
+        except GoodCart.DoesNotExist:
+            cart = None
+
+        return render(self.request, 'app_shop/cart.html', context={'cart': cart, 'total_price': total_price})
+
+@require_POST
+@login_required(login_url='login', redirect_field_name='main')
+def add_good_to_cart(request, *args, **kwargs):#7 (06.02.)
+    good = get_object_or_404(Good, pk=kwargs['pk'])
+    form = CartAddForm(request.POST)
+    if form.is_valid():
+        good_num = form.cleaned_data['good_num']
+        if good_num == 0 or good_num > good.amount:
+            messages.add_message(request, messages.INFO, 'Invalid good_num')
+            return redirect('main')
+        with transaction.atomic():
+            GoodCart.objects.create(good=good, user=request.user, good_num=good_num)
+            good.sub_amount(good_num)
+    return redirect('cart')
 
 class CustomLoginView(LoginView): #2
     template_name = 'app_shop/login.html'
